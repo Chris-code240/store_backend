b3f1aa557583e969e5489036fac12d3f533c4c26
Christian Duah <82686181+Chris-code240@users.noreply.github.com>
2024-10-22 14:19:39 +0100

    Update Database.php

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@
 class Database {
     public function connect(){
         try {
-            $con = new PDO('mysql:host=sql3.freesqldatabase.com;port=3306;dbname=sql3739839;', 'sql3739839', '9QlI3fmUBN');
+            $con = new PDO('mysql:host=mysql-chris240.alwaysdata.net;dbname=chris240_store;', 'chris240', ')N*6GA=NLc3dH&=');
         $con->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::ATTR_ERRMODE);
         return $con;
         } catch (PDOException $th) {
