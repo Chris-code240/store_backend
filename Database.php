ecc0a5ef623d3372bd016ac8d54bd8c1b8d2dd95
Chris-code240 <duah.marfochristian@gmail.com>
2024-10-22 12:00:05 +0100

    database

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@
 class Database {
     public function connect(){
         try {
-            $con = new PDO('mysql:host=localhost;port=3306;dbname=store;',$username = 'root', $password = 'root');
+            $con = new PDO('mysql:host=sql3.freesqldatabase.com;port=3306;dbname=sql3739839;', 'sql3739839', '9QlI3fmUBN');
         $con->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::ATTR_ERRMODE);
         return $con;
         } catch (PDOException $th) {
